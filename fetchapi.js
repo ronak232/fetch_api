ad54f2ca515581f5f50fb114c127964d8c62f75d
Ronak <ronakmathur9@gmail.com>
2022-09-02 19:29:25 +0530

    Final Changes

--- fetchapi.js
+++ fetchapi.js
@@ -1,6 +1,7 @@
 // This is to get data from api.
 
 console.log("To Fetch the data click the fetch button.");
+let dataToshow = document.getElementById("output");
 
 let data = document
   .getElementById("btn")
@@ -14,7 +15,32 @@ let data = document
       })
       // if the data is successfully fetch the data the
       .then((data) => {
-        console.log(data);
+        console.log(JSON.stringify(data));
+
+        let table = `
+        <table>
+        <th>
+        <tr>
+        <td>ID</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>E-Mail</td>
+        </th>
+        </tr>
+        <tr>
+        <td>${data.id}</td>
+        <td>${data.first_name}</td>
+        <td>${data.last_name}</td>
+        <td>${data.email}</td>
+        <td><img src = "${data.avatar}"></td>
+        </tr>
+        </table>
+                                              `;
+        dataToshow.innerHTML = table;
+
+        setTimeout(() => {
+          console.log("Fetched data Sucessfully!");
+        }, 3000);
       })
       // if the data caught some error then move to reject state.
       .catch((error) => {
@@ -22,9 +48,6 @@ let data = document
           console.log("The api is not valid.", error);
         }
       });
-    setTimeout(() => {
-      console.log("Fetched data Sucessfully!");
-    }, 3000);
   });
 
 // To post the data of api
