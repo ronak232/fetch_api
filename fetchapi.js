c3e64ed63deeedef62e4c1181a3753662ae45b9c
Ronak <ronakmathur9@gmail.com>
2022-09-02 16:38:21 +0530

    Initial Commit

--- fetchapi.js
+++ fetchapi.js
@@ -1,26 +1,30 @@
+// This is to get data from api.
+
 console.log("To Fetch the data click the fetch button.");
 
-let data = document.getElementById("btn").addEventListener("click", () => {
-  let url = "https://random-data-api.com/api/v2/users";
-  fetch(url)
-    //First is that promise is in the pending state. Once the api response is sucessful.
-    .then((resp) => {
-      return resp.json();
-    })
-    // if the data is successfully fetch the data the
-    .then((data) => {
-      console.log(data);
-    })
-    // if the data caught some error then move to reject state.
-    .catch((error) => {
-      if (error === 404) {
-        console.log("The api is not valid.", error);
-      }
-    });
+let data = document
+  .getElementById("btn")
+  .addEventListener("click", async () => {
+    let url = "https://random-data-api.com/api/v2/users";
+    await fetch(url)
+      //First is that promise is in the pending state. Once the api response is sucessful.
+      .then((resp) => {
+        let jsonData = resp.json();
+        return jsonData;
+      })
+      // if the data is successfully fetch the data the
+      .then((data) => {
+        console.log(data);
+      })
+      // if the data caught some error then move to reject state.
+      .catch((error) => {
+        if (error === 404) {
+          console.log("The api is not valid.", error);
+        }
+      });
     setTimeout(() => {
       console.log("Fetched data Sucessfully!");
     }, 3000);
-});
+  });
 
-// fetchData();
-
+// To post the data of api
